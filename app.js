f516561b1669cf4a9e6e426f4ab9d7096bcced6a
Dean Terrizzi <dterrizzi@outlook.com>
2020-05-06 01:10:44 -0400

    additional comment styling, also fixed paths and added main.css

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var express = require("express"),
     Comment = require("./models/comment"),
     seedDB = require("./seeds");
 
-seedDB();
-
 mongoose.connect("mongodb://localhost/yelp_camp", {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -15,6 +13,8 @@ mongoose.connect("mongodb://localhost/yelp_camp", {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
 
 app.get("/", function(req, res){
     res.render("landing");
