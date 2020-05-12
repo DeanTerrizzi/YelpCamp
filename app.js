86ef052521d5c73d6f6b8274487fc1f55d57b400
Dean Terrizzi <dterrizzi@outlook.com>
2020-05-12 01:42:31 -0400

    configured passport, added register routes, added register template

--- app.js
+++ app.js
@@ -19,6 +19,19 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// Passport config
+app.use(require("express-session")({
+    secret: "Anything I Want",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -107,6 +120,29 @@ app.post("/campgrounds/:id/comments", function(req, res){
     // create new comment
     // connect new comment to campground
     // redirect campground show page
+});
+
+//==============
+// Auth Routes
+//==============
+
+// Show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
 });
 
 app.listen(3000, function(){
